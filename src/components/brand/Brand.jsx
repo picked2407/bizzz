32a162c2e01a86642a0af0177ec0035a47d5596a
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-01-18 00:04:13 +0530

    Update Brand.jsx

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,24 +1,10 @@
+// brand.js (or whatever your file name is)
 import React from 'react';
-import { google, slack, atlassian, dropbox, shopify } from './imports';
 import './brand.css';
 
 const Brand = () => (
   <div className="gpt3__brand section__padding">
-    <div>
-      <img src={google} />
-    </div>
-    <div>
-      <img src={slack} />
-    </div>
-    <div>
-      <img src={atlassian} />
-    </div>
-    <div>
-      <img src={dropbox} />
-    </div>
-    <div>
-      <img src={shopify} />
-    </div>
+    {/* Content removed */}
   </div>
 );
 
