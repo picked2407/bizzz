aab9a0eb4d0fbae1b7a7200ac572427a8e793b3f
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-01-18 00:01:41 +0530

    Update Footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,45 +1,36 @@
 import React from 'react';
-import gpt3Logo from '../../logo.svg';
 import './footer.css';
 
 const Footer = () => (
   <div className="gpt3__footer section__padding">
     <div className="gpt3__footer-heading">
-      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      <h1 className="gradient__text">Join us to start your journey today</h1>
     </div>
 
     <div className="gpt3__footer-btn">
-      <p>Request Early Access</p>
+      <p>Get started</p>
     </div>
 
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
-        <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+        <p>BizHub</p>
       </div>
       <div className="gpt3__footer-links_div">
-        <h4>Links</h4>
-        <p>Overons</p>
-        <p>Social Media</p>
-        <p>Counters</p>
-        <p>Contact</p>
+        <p><a href="https://docs.google.com/document/d/1kRbODawwBtwyVrR_gn2B49J0sRji7al7XW2kOc1AGs4/edit">Terms of Service</a></p>
+        <p><a href="https://docs.google.com/document/d/1tsVfBbB5tfQFtsUCgC4cm-r2YzlOMqY0qhoJ4rWt4DY/edit">Discord Freelancer Manual</a></p>
       </div>
       <div className="gpt3__footer-links_div">
-        <h4>Company</h4>
-        <p>Terms & Conditions </p>
-        <p>Privacy Policy</p>
-        <p>Contact</p>
+        <p><a href="https://docs.google.com/document/d/1Y4zrLmmczuJ2egBqIKD--kRuUAWGaGPuKiImIjs23is/edit">Freelancer Agreement </a></p>
+        <p><a href="https://docs.google.com/document/d/e/2PACX-1vSHeHAZTYQ-TBLRgg3wys-7WIP2Djype6sgDYcEejEZIJenLsMXc-7yVCOcsiWjKaJAg90lgqtneYcR/pub">Business Plan</a></p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@payme.net</p>
+        <p>bizhubgeneral@gmail.com</p>
       </div>
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>BizHub. All rights reserved</p>
     </div>
   </div>
 );
