8635891c9e56b78b0a831d11f1bd2cecc7509d47
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-01-18 00:01:22 +0530

    Update Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,17 +6,12 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-
-      <div className="gpt3__header-content__input">
-        <input type="email" placeholder="Your Email Address" />
-        <button type="button">Get Started</button>
-      </div>
+      <h1 className="gradient__text">Global talent at your doorstep.</h1>
+      <p>Make your next big thing come to life by leveraging the global talent that we provide. We make it easy for you to choose from a wide variety of talent that suit your needs best, at the price-point that you are comfortable with, within the time constraints that you desire.</p>
 
       <div className="gpt3__header-content__people">
         <img src={people} />
-        <p>1,600 people requested access a visit in last 24 hours</p>
+        <p>60+ people joined us in last 24 hours</p>
       </div>
     </div>
 
