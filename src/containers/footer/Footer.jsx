9ef0d5a7d268dd4fe654891350d8ca6e224bd702
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-01-20 01:10:38 +0530

    Update Footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -16,8 +16,8 @@ const Footer = () => (
         <p>BizHub</p>
       </div>
       <div className="gpt3__footer-links_div">
-        <p><a href="https://docs.google.com/document/d/1kRbODawwBtwyVrR_gn2B49J0sRji7al7XW2kOc1AGs4/edit">Terms of Service</a></p>
-        <p><a href="https://docs.google.com/document/d/1tsVfBbB5tfQFtsUCgC4cm-r2YzlOMqY0qhoJ4rWt4DY/edit">Discord Freelancer Manual</a></p>
+        <p><a href="https://docs.google.com/document/d/e/2PACX-1vSeFusrKMkEyUz6jsptxK-4rTe50TNSrMGYICsQecTcDV60YVY-f9Io_hzwfzWG79glAdj6HxrVMX_n/pub">Terms of Service</a></p>
+        <p><a href="https://docs.google.com/document/d/e/2PACX-1vSZdjP-FOLjXYjYNhbXUd5RLh5RusMa7mYoyiXxSL7Fdcn5h9FCLgbHvWQaYfELdtlTlgL2hLUJeORI/pub">Discord Freelancer Manual</a></p>
         <p><a href="https://docs.google.com/document/d/1Y4zrLmmczuJ2egBqIKD--kRuUAWGaGPuKiImIjs23is/edit">Freelancer Agreement </a></p>
       </div>
       <div className="gpt3__footer-links_div">
